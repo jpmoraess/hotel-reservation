6af79dfb563d89ec68baf403dd86e449dd006fdd
jpmoraess <moraess.it@gmail.com>
2024-04-20 10:51:19 -0300

    adding the database layer

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"context"
 	"flag"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jpmoraess/hotel-reservation/api"
+	"github.com/jpmoraess/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const dbURI = "mongodb://localhost:27017"
+
+var config = fiber.Config{
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		return ctx.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":8080", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
+	if err != nil {
+		panic(err)
+	}
+
+	// handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiV1 := app.Group("/api/v1")
 
-	apiV1.Get("/user", api.HandleGetUsers)
-	apiV1.Get("/user/:id", api.HandleGetUser)
+	apiV1.Get("/user", userHandler.HandleGetUsers)
+	apiV1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
